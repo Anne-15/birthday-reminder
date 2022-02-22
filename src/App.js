fcff1618ddc70d18043006701e08850bf3fa6106
Anne-15 <wariiyuanne@gmail.com>
2022-02-22 15:21:02 +0300

    creating a list of people

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import data from "./data";
 import List from "./list";
 
 function App(){
-  return <h2>Reminder project setup</h2>
+  const [people, setPeople] = useState(data);
+  return(
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people = {people}/>
+        <button onclick={() => console.log('I was clicked')}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
